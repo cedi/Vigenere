811007cce716b550af51e6becbfa38228d678513
cedi <c_edi@gmx.de>
2014-10-09 10:49:23 +0200

    Initial Commit

---
+++ Vigenere/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Vigenere
+//
+//  Created by Cedric Kienzler on 09.10.14.
+//  Copyright (c) 2014 Cedric Kienzler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
